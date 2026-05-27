ac382cea1b9cfc1a7934d4d38a8297cd19090b14
Taqdees Arshad <taqdees795@gmail.com>
2026-05-27 22:53:21 +0500

    analytics updated

--- obj/Debug/net8.0/SmartCityPulse.AssemblyInfo.cs
+++ obj/Debug/net8.0/SmartCityPulse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartCityPulse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+088116e8d7f406c0fa2b41b8a276597cbf0e8463")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d609a650c1755b1d7812e44e7f8b2f639923a156")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartCityPulse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartCityPulse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
