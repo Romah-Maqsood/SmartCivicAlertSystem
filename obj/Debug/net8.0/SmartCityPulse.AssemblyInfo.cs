a5ea8cf63725a30fd4f85763c5b309052c06a36b
Taqdees Arshad <taqdees795@gmail.com>
2026-05-29 01:53:00 +0500

    view incident detail UI improved

--- obj/Debug/net8.0/SmartCityPulse.AssemblyInfo.cs
+++ obj/Debug/net8.0/SmartCityPulse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartCityPulse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f81814e08dd373efb8fcf2bfb4dd47edf841bee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0e6715fc78ede3f370e323834c2e973a061d2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartCityPulse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartCityPulse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
